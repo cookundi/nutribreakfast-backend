73da7b90ab697bc6630d29e517db6fe49a3ca52d
tech-genius <adebimpehabeeb10@gmail.com>
2025-12-13 16:02:46 +0100

    edit email port

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -8,7 +8,7 @@ const logger = require('../utils/logger');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
